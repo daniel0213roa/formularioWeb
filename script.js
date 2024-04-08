dd6adf1cbdb29f543ab5cadf9a562190094e915d
Daniel Roa <69731488+daniel0213roa@users.noreply.github.com>
2024-04-08 11:46:40 -0500

    actualizacion

---
+++ script.js
@@ -0,0 +1,25 @@
+const form = document.forms['loginForm'];
+
+form.addEventListener('submit', function handleFormSubmit(event){
+event.preventDefult();
+
+const email = form['email'].value;
+const password = form['password'].value;
+
+loginUser(email,password);
+
+
+});
+
+function loginUser(email, password) {
+
+    console.log('Loging user '+email);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+      console.log('Credenciales correctas, ¡bienvenido! ' + user);
+      localStorage.setItem('user', JSON.stringify(user));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
